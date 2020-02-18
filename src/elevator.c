02b5936046feeed44d868df06ee49a0612b489c6
Torjus Bakkene <torjus.bakkene@gmail.com>
2020-02-18 10:52:27 +0100

    Enable moving back to previous floor after estop

--- src/elevator.c
+++ src/elevator.c
@@ -4,6 +4,9 @@
 static ElevatorState m_elevator_current_state;
 static int m_elevator_current_target;
 static int m_elevator_current_floor;
+
+// Last floor and movement direction must be remembered in case elevator is e-stopped between floors
+static HardwareMovement m_elevator_last_movement_direction;
 static int m_elevator_last_floor;
 
 
@@ -58,6 +61,7 @@ int elevator_init()
     m_elevator_current_state = ELEVATOR_STATE_INIT;
     m_elevator_current_floor = -1;
     m_elevator_current_target = -1;
+    m_elevator_last_movement_direction = HARDWARE_MOVEMENT_STOP;
     return 0;
 }
 
@@ -80,6 +84,8 @@ int elevator_update_floor_status()
     return 0;
 }
 
+// TODO: Reference document
+// This function should not be read without its acompanying state diagram, found on page {PAGE} in rapport.pdf
 int elevator_update_state()
 {
     // ESTOP takes priority, but only if not already estopped
@@ -99,6 +105,7 @@ int elevator_update_state()
         case ELEVATOR_STATE_INIT:
             hardware_command_movement(HARDWARE_MOVEMENT_DOWN);
             m_elevator_current_state = ELEVATOR_STATE_INIT_MOVING_DOWN;
+            m_elevator_last_movement_direction = HARDWARE_MOVEMENT_DOWN;
             break;
 
         case ELEVATOR_STATE_INIT_MOVING_DOWN:
@@ -106,52 +113,56 @@ int elevator_update_state()
             {
                 hardware_command_movement(HARDWARE_MOVEMENT_STOP);
                 m_elevator_current_state = ELEVATOR_STATE_IDLE;
+                m_elevator_last_movement_direction = HARDWARE_MOVEMENT_STOP;
             }
             break;
 
         case ELEVATOR_STATE_IDLE:
-            // Uses last_floor instead of current_floor because the elevator can be stopped
-            // by the stop button outside of a floor
             if (m_elevator_current_target != -1)
             {
-                if (m_elevator_current_target != m_elevator_last_floor)
+                if (m_elevator_current_target != m_elevator_current_floor)
                 {
-                    if (m_elevator_current_target < m_elevator_last_floor)
+                    // Uses last_floor + last_movement modifier instead of current_floor because 
+                    // the elevator can be stopped by the stop button between floors
+                    if ((float)m_elevator_current_target < (float)m_elevator_last_floor - 0.5*((float)m_elevator_last_movement_direction - 1))
                     {
                         hardware_command_movement(HARDWARE_MOVEMENT_DOWN);
                         m_elevator_current_state = ELEVATOR_STATE_MOVING_DOWN;
+                        m_elevator_last_movement_direction = HARDWARE_MOVEMENT_DOWN;
                     } else
                     {
                         hardware_command_movement(HARDWARE_MOVEMENT_UP);
                         m_elevator_current_state = ELEVATOR_STATE_MOVING_UP;
+                        m_elevator_last_movement_direction = HARDWARE_MOVEMENT_UP;
                     }
                 }else
                 {
                     // Elevator is already at target
-                    // TODO: Remove target
-                    // orders_clear_target(int m_elevator_last_floor);
+                    elevator_clear_target();
                 }
             }
             break;
 
         case ELEVATOR_STATE_MOVING_DOWN:
-            if (m_elevator_current_target == m_elevator_last_floor)
+            if (m_elevator_current_target == m_elevator_current_floor)
             {
                 hardware_command_movement(HARDWARE_MOVEMENT_STOP);
                 m_elevator_current_state = ELEVATOR_STATE_STOPPED_AT_FLOOR;
+                m_elevator_last_movement_direction = HARDWARE_MOVEMENT_STOP;
             }
             break;
 
         case ELEVATOR_STATE_MOVING_UP:
-            if (m_elevator_current_target == m_elevator_last_floor)
+            if (m_elevator_current_target == m_elevator_current_floor)
             {
                 hardware_command_movement(HARDWARE_MOVEMENT_STOP);
                 m_elevator_current_state = ELEVATOR_STATE_STOPPED_AT_FLOOR;
+                m_elevator_last_movement_direction = HARDWARE_MOVEMENT_STOP;
             }
             break;
 
         case ELEVATOR_STATE_STOPPED_AT_FLOOR:
-            if (m_elevator_current_target == m_elevator_last_floor)
+            if (m_elevator_current_target == m_elevator_current_floor)
             {
                 // Just arrived
                 elevator_clear_target();
@@ -268,6 +279,8 @@ int elevator_update_orders()
         elevator_add_order_if_button_pressed(floor, HARDWARE_ORDER_INSIDE);
         elevator_add_order_if_button_pressed(floor, HARDWARE_ORDER_DOWN);
     }
+    // TODO: Integrate with orders module
+    //orders_get_new_target(int current_floor);
     return 0;
 }
 
