f1c9c8eda746efc72372ea0ffa23229d8f54cf8d
Torjus Bakkene <torjus.bakkene@gmail.com>
2020-02-25 11:08:29 +0100

    Change function names and add doxygen comments

--- src/orders.c
+++ src/orders.c
@@ -4,69 +4,69 @@
 void init_orders(){
   orders_up = vector_create(); 
   orders_down = vector_create();
-  target_buffer = vector_create();
-  target = -1; 
+  orders_target_buffer = vector_create();
+  orders_target = -1; 
 }
 
 
 
-int orders_get_new_target(HardwareMovement hardware_movement, double curr_floor){
+int orders_get_new_target(HardwareMovement hardware_movement, double current_floor){
   if (hardware_movement == HARDWARE_MOVEMENT_UP){
-    if(target == -1)
+    if(orders_target == -1)
     {
       //ESTOP
-      return orders_get_new_target(HARDWARE_MOVEMENT_STOP, curr_floor+0.5);
+      return orders_get_new_target(HARDWARE_MOVEMENT_STOP, current_floor+0.5);
     }
     int orders_up_size = vector_size(orders_up);
     //Going through orders_up to see if we can do some stops on the way
-    //And adding them to target
+    //And adding them to orders_target
     for (int i = 0; i < orders_up_size; i++){
-      if (curr_floor < orders_up[i] && target > orders_up[i]){
-	      target = orders_up[i]; 
-	      vector_remove(orders_up, i);
-      	update_target_buffer(target);
-	      i--;
-	      orders_up_size--;
+      if (current_floor < orders_up[i] && orders_target > orders_up[i]){
+	orders_target = orders_up[i]; 
+	vector_remove(orders_up, i);
+	orders_add_to_target_buffer(orders_target);
+	i--;
+	orders_up_size--;
       }
     }
-    return target;
+    return orders_target;
   }
   else if (hardware_movement == HARDWARE_MOVEMENT_DOWN){
-    if(target == -1)
+    if(orders_target == -1)
     {
       //ESTOP
-      return orders_get_new_target(HARDWARE_MOVEMENT_STOP, curr_floor-0.5);
+      return orders_get_new_target(HARDWARE_MOVEMENT_STOP, current_floor-0.5);
     }
     int orders_down_size = vector_size(orders_down);
     //Going through orders_down to see if we can do some stops on the way
-    //And adding them to targets
+    //And adding them to orders_targets
     for (int i = 0; i < orders_down_size; i++){
-      if (curr_floor > orders_down[i] && target < orders_down[i]){
-	      target = orders_down[i]; 
-	      vector_remove(orders_down, i);
-	      update_target_buffer(target);
-	      i--;
-	      orders_down_size--;
+      if (current_floor > orders_down[i] && orders_target < orders_down[i]){
+	orders_target = orders_down[i]; 
+	vector_remove(orders_down, i);
+	orders_add_to_target_buffer(orders_target);
+	i--;
+	orders_down_size--;
       }
     }
-    return target;
+    return orders_target;
   }
 
-  //Checking if target is empty, and new target have to be included. 
+  //Checking if orders_target is empty, and new orders_target have to be included. 
   
-    int buffer_size = vector_size(target_buffer);
+    int buffer_size = vector_size(orders_target_buffer);
     if (buffer_size > 0){
-      bubbleSort(target_buffer, buffer_size);
+      bubbleSort(orders_target_buffer, buffer_size);
       if (hardware_movement == HARDWARE_MOVEMENT_DOWN){
-        target = target_buffer[buffer_size];
+        orders_target = orders_target_buffer[buffer_size];
       }else if (hardware_movement == HARDWARE_MOVEMENT_UP){
-        target = target_buffer[0];
+        orders_target = orders_target_buffer[0];
       }else 
-      target = target_buffer[0]; 
+      orders_target = orders_target_buffer[0]; 
     }
 
     else if (buffer_size == 0){
-      //Find closest order, and set to target
+      //Find closest order, and set to orders_target
       int orders_up_size = vector_size(orders_up);
       int orders_down_size = vector_size(orders_down);
 
@@ -75,38 +75,38 @@ int orders_get_new_target(HardwareMovement hardware_movement, double curr_floor)
 	    return -1;
       } else if (orders_down_size == 0)
       {
-	      // Only orders going up
-	      //Find closest floor to the elevator
-	      int closest_floor_up = orders_up[0];	
-        int order_up_index = 0;
-	        for (int i = 1; i < orders_up_size; i++){
-	          if (abs(closest_floor_up - curr_floor) > abs(orders_up[i] - curr_floor)){
+	// Only orders going up
+	//Find closest floor to the elevator
+	int closest_floor_up = orders_up[0];	
+	int order_up_index = 0;
+	for (int i = 1; i < orders_up_size; i++){
+	  if (abs(closest_floor_up - current_floor) > abs(orders_up[i] - current_floor)){
 
-	           closest_floor_up = orders_up[i];
-	           order_up_index = i;
-	          }
-	        }
-	    vector_remove(orders_up, order_up_index);
-	    target = closest_floor_up;
-    	update_target_buffer(target);
-	    return target;
+	    closest_floor_up = orders_up[i];
+	    order_up_index = i;
+	  }
+	}
+	vector_remove(orders_up, order_up_index);
+	orders_target = closest_floor_up;
+	orders_add_to_target_buffer(orders_target);
+	return orders_target;
       } else if (orders_up_size == 0)
       {
-	    // Only orders going down
-    	//Find closest floor to the elevator
-	    int closest_floor_down = orders_down[0];	
-    	int order_down_index = 0;
-	      for (int i = 1; i < orders_down_size; i++){
-	        if (abs(closest_floor_down - curr_floor) > abs(orders_down[i] - curr_floor)){
+	// Only orders going down
+	//Find closest floor to the elevator
+	int closest_floor_down = orders_down[0];	
+	int order_down_index = 0;
+	for (int i = 1; i < orders_down_size; i++){
+	  if (abs(closest_floor_down - current_floor) > abs(orders_down[i] - current_floor)){
 
-	          closest_floor_down = orders_down[i];
-	          order_down_index = i;
-	        }
-	      }
-	    vector_remove(orders_down, order_down_index);
-	    target = closest_floor_down;
-	    update_target_buffer(target);
-	    return target;
+	    closest_floor_down = orders_down[i];
+	    order_down_index = i;
+	  }
+	}
+	vector_remove(orders_down, order_down_index);
+	orders_target = closest_floor_down;
+	orders_add_to_target_buffer(orders_target);
+	return orders_target;
       } else 
       {
 	// There are both orders going up and down
@@ -117,14 +117,14 @@ int orders_get_new_target(HardwareMovement hardware_movement, double curr_floor)
 
 	//Find closest order to the elevator
 	for (int i = 1; i < orders_up_size; i++){
-	  if (abs(closest_floor_up - curr_floor) > abs(orders_up[i] - curr_floor)){
+	  if (abs(closest_floor_up - current_floor) > abs(orders_up[i] - current_floor)){
 
 	    closest_floor_up = orders_up[i];
 	    order_up_index = i;
 	  }
 	}
 	for (int i = 1; i < orders_down_size; i++){
-	  if (abs(closest_floor_down - curr_floor) > abs(orders_down[i] - curr_floor)){
+	  if (abs(closest_floor_down - current_floor) > abs(orders_down[i] - current_floor)){
 
 	    closest_floor_down = orders_down[i];
 	    order_down_index = i; 
@@ -132,43 +132,42 @@ int orders_get_new_target(HardwareMovement hardware_movement, double curr_floor)
 	}
 	
 	// Compare closest orders
-	if (abs(closest_floor_down - curr_floor) < abs(closest_floor_up-curr_floor)){
-	  target = closest_floor_down;
-	  update_target_buffer(target);
+	if (abs(closest_floor_down - current_floor) < abs(closest_floor_up-current_floor)){
+	  orders_target = closest_floor_down;
+	  orders_add_to_target_buffer(orders_target);
 	  vector_remove(orders_down, order_down_index);
-	  return target;
+	  return orders_target;
 	}
-	else if (abs(closest_floor_down - curr_floor) > abs(closest_floor_up-curr_floor)){
-	  target = closest_floor_up;
-	  update_target_buffer(target);
+	else if (abs(closest_floor_down - current_floor) > abs(closest_floor_up-current_floor)){
+	  orders_target = closest_floor_up;
+	  orders_add_to_target_buffer(orders_target);
 	  vector_remove(orders_up, order_up_index);
-	  return target;
+	  return orders_target;
 	}
-	else if (abs(closest_floor_down - curr_floor) == abs(closest_floor_up-curr_floor)){
-	  target = closest_floor_up;
-	  update_target_buffer(target);
+	else if (abs(closest_floor_down - current_floor) == abs(closest_floor_up-current_floor)){
+	  orders_target = closest_floor_up;
+	  orders_add_to_target_buffer(orders_target);
 	  vector_remove(orders_up, order_up_index); 
-	  return target;
+	  return orders_target;
 	}
       }
     }
   
-  return target;
+  return orders_target;
 }
 
 void orders_add_order(HardwareOrder hardware_order, int floor){
   switch (hardware_order)
   {
     case HARDWARE_ORDER_UP:
-      update_orders_up(floor);
+      orders_add_to_orders_up(floor);
       break;
     case HARDWARE_ORDER_DOWN:
-      update_orders_down(floor);
+      orders_add_to_orders_down(floor);
       break;
     case HARDWARE_ORDER_INSIDE:
-      //update_target_buffer(floor);
-      update_orders_down(floor);
-      update_orders_up(floor);
+      orders_add_to_orders_down(floor);
+      orders_add_to_orders_up(floor);
       break;
     default:
       break;
@@ -176,20 +175,20 @@ void orders_add_order(HardwareOrder hardware_order, int floor){
   return;
 }
 
-void update_target_buffer(int floor){
-  int target_buffer_size = vector_size(target_buffer);
-  for (int i = 0; i < target_buffer_size; i++)
+void orders_add_to_target_buffer(int floor){
+  int orders_target_buffer_size = vector_size(orders_target_buffer);
+  for (int i = 0; i < orders_target_buffer_size; i++)
   {
     //Don't add duplicates
-    if (target_buffer[i] == floor)
+    if (orders_target_buffer[i] == floor)
       return;
   }
-  vector_add(&target_buffer, floor);
+  vector_add(&orders_target_buffer, floor);
 }
 
 
 //Updating the list orders_up when the up button is pressed on some floor
-void update_orders_up(int floor){
+void orders_add_to_orders_up(int floor){
   int orders_up_size = vector_size(orders_up);
   for (int i = 0; i < orders_up_size; i++)
   {
@@ -201,7 +200,7 @@ void update_orders_up(int floor){
 }
 
 //Updating the list orders_down when the down button is pressed on some floor
-void update_orders_down(int floor){
+void orders_add_to_orders_down(int floor){
   int orders_down_size = vector_size(orders_down);
   for (int i = 0; i < orders_down_size; i++)
   {
@@ -232,18 +231,18 @@ void orders_clear_target(int floor){
       orders_down_size--;
     }
   }
-  int target_buffer_size = vector_size(target_buffer);
-  for(int i = 0; i < target_buffer_size; i++)
+  int orders_target_buffer_size = vector_size(orders_target_buffer);
+  for(int i = 0; i < orders_target_buffer_size; i++)
   {
-    if (target_buffer[i] == floor)
+    if (orders_target_buffer[i] == floor)
     {
-      vector_remove(target_buffer, i--);
-      target_buffer_size--;
+      vector_remove(orders_target_buffer, i--);
+      orders_target_buffer_size--;
     }
   }
-  if (target == floor)
+  if (orders_target == floor)
   {
-    target = -1;
+    orders_target = -1;
   }
   return;
 }
@@ -252,10 +251,11 @@ void orders_clear_target(int floor){
 void orders_clear_all(){
   vector_free(orders_up);
   vector_free(orders_down);
-  vector_free(target_buffer);
+  vector_free(orders_target_buffer);
   init_orders(); 
 }
 
+// Helper functions
 void swap(int *xp, int *yp)  
 {  
   int temp = *xp;  
@@ -263,7 +263,6 @@ void swap(int *xp, int *yp)
   *yp = temp;  
 }  
 
-// A function to implement bubble sort  
 void bubbleSort(int arr[], int n)  
 {  
   int i, j;  
--- src/orders.h
+++ src/orders.h
@@ -1,30 +1,68 @@
+/**
+   @file
+   @brief A module for keeping track of orders
+   */
 #ifndef orders_h 
 #define orders_h 
 
+#include <math.h>
 #include "vec.h"
 #include "driver/hardware.h"
 
-//Some initializing if needed
+/**
+  * @brief Initializes the orders module. Only needs to be called once at the beginning of the program
+  */
 void init_orders(); 
 
-//Updating the list target_buffer
-void update_target_buffer(int floor); 
+/**
+  * @brief Adds the floor to the orders_target buffer, but discards duplicates
+  * 
+  * This function is primarily for internal use, and programs using the module should call orders_add_order instead
+  * @param floor The floor number to add as a orders_target. 0-indexed
+  */
+void orders_add_to_target_buffer(int floor); 
 
-//Updating the list orders_up when the up button is pressed on some floor
-void update_orders_up(int floor);
+/**
+  * @brief Adds the floor to the orders_up buffer, but discards duplicates
+  * @param floor The floor number to add. 0-indexed
+  */
+void orders_add_to_orders_up(int floor);
 
-//Updating the list orders_down when the down button is pressed on some floor
-void update_orders_down(int floor); 
+/**
+  * @brief Adds the floor to the orders_down buffer, but discards duplicates
+  * @param floor The floor number to add. 0-indexed
+  */
+void orders_add_to_orders_down(int floor); 
 
 
+
+/**
+  * @brief Generic wrapper function for adding orders. This is the recommended way to add orders
+  * @param hardware_order The order type (Which panel was the elevator ordered from?)
+  * @param floor The floor number to add. 0-indexed
+  */
 void orders_add_order(HardwareOrder hardware_order, int floor);
 
+
+/**
+  * @brief Clears the current orders_target. To be used when the elevator successfully handles an order
+  * @param floor The floor number to clear. 0-indexed
+  */
 void orders_clear_target(int floor);
 
+/**
+  * @brief Clears all entries from all buffers. To be called when the elevator makes an emergency stop
+  */
 void orders_clear_all(); 
 
 
-int orders_get_new_target(HardwareMovement hardware_movement, double curr_floor);
+/**
+  * @brief Requests a new orders_target from the buffer
+  * @param hardware_movement The current movement direction of the elevator
+  * @param current_floor The current posision of the elevator. 0-indexed. This is a double because the elevator can be between floors after an emergency stop
+  * @returns The floor number of the floor the elevator should move to
+  */
+int orders_get_new_target(HardwareMovement hardware_movement, double current_floor);
 
 
 void bubbleSort(int arr[], int n); 
@@ -34,9 +72,8 @@ void swap(int *xp, int *yp);
 
 int* orders_up  ;
 int* orders_down; 
-int* targets; 
-int* target_buffer; 
-int target; 
+int* orders_target_buffer; 
+int orders_target; 
 
 
 
